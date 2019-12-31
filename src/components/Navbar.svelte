880963b39f946a4702fea69186c78e14b511080d
Jordon13 <reamxtek@gmail.com>
2019-12-31 06:34:17 -0500

    build config

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -70,6 +70,21 @@
     
 </style>
 
+<svelte:head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+	 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+</svelte:head>
+
 <div class="navbar-fixed">
     <nav class="navElements">
         <div class="nav-wrapper">
